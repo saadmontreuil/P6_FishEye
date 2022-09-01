d7833c91ef917f1547432dc17971b9af54fd8502
saadmontreuil <79247132+saadmontreuil@users.noreply.github.com>
2022-09-01 17:12:21 +0200

    Display Photographers in home page

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,17 +1,33 @@
-function photographerFactory(data) {
-    const { name, portrait } = data;
+class Photographer {
+  constructor(data) {
+    this.name = data.name;
+    this.id = data.id;
+    this.city = data.city;
+    this.country = data.country;
+    this.tagline = data.tagline;
+    this.price = data.price;
+    this.portrait = data.portrait;
+  }
 
-    const picture = `assets/photographers/${portrait}`;
+  PhotographerCard() {
+    const wrapper = document.querySelector('.photographer_section');
+    const article = document.createElement('article');
 
-    function getUserCardDOM() {
-        const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
-        h2.textContent = name;
-        article.appendChild(img);
-        article.appendChild(h2);
-        return (article);
-    }
-    return { name, picture, getUserCardDOM }
+    const html = `
+        <a href="photographer.html?id=${this.id}">
+        <div class="photographer-link">
+          <img src="assets/photographers/${this.portrait}" alt="${this.name}">
+          <h2>${this.name}</h2>
+        </div>
+        </a>
+        <p>
+          <span class="location">${this.city}, ${this.country}</span><br>
+          <span class="tagline">${this.tagline}</span><br>
+          <span class="price">${this.price}€/Jour</span><br>
+        </p>
+      `;
+
+    article.innerHTML = html;
+    wrapper.appendChild(article);
+  }
 }
--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,45 +1,14 @@
-    async function getPhotographers() {
-        // Penser à remplacer par les données récupérées dans le json
-        const photographers = [
-            {
-                "name": "Ma data test",
-                "id": 1,
-                "city": "Paris",
-                "country": "France",
-                "tagline": "Ceci est ma data test",
-                "price": 400,
-                "portrait": "account.png"
-            },
-            {
-                "name": "Autre data test",
-                "id": 2,
-                "city": "Londres",
-                "country": "UK",
-                "tagline": "Ceci est ma data test 2",
-                "price": 500,
-                "portrait": "account.png"
-            },
-        ]
-        // et bien retourner le tableau photographers seulement une fois
-        return ({
-            photographers: [...photographers, ...photographers, ...photographers]})
-    }
+const getPhotographers = async () => fetch('data/photographers.json')
+  .then((res) => res.json())
+  .catch((err) => console.log('error', err));
 
-    async function displayData(photographers) {
-        const photographersSection = document.querySelector(".photographer_section");
+const init = async () => {
+  const { photographers } = await getPhotographers();
 
-        photographers.forEach((photographer) => {
-            const photographerModel = photographerFactory(photographer);
-            const userCardDOM = photographerModel.getUserCardDOM();
-            photographersSection.appendChild(userCardDOM);
-        });
-    };
+  photographers.forEach((photographer) => {
+    const Template = new Photographer(photographer);
+    Template.PhotographerCard();
+  });
+};
 
-    async function init() {
-        // Récupère les datas des photographes
-        const { photographers } = await getPhotographers();
-        displayData(photographers);
-    };
-    
-    init();
-    
+init();
