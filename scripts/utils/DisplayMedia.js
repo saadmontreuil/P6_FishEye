af1c6b6246a7cea8c2580c4db5c51e92a07bc215
saadmontreuil <79247132+saadmontreuil@users.noreply.github.com>
2022-09-14 14:02:10 +0200

    display media in photographer page + style

--- scripts/utils/DisplayMedia.js
+++ scripts/utils/DisplayMedia.js
@@ -5,5 +5,18 @@ class DisplayMedia {
     this.mediaWrapper = document.querySelector('.photograph-media');
   }
 
-  
+  displayMedia() {
+    this.media.forEach((media) => {
+      const medium = new MediaFactory(media, this.photographerName);
+      const selectdata = medium.data;
+      console.log(selectdata);
+      const element = medium.createMedia();
+      console.log(medium);
+      if (element !== undefined){
+
+        this.mediaWrapper.appendChild(element.creatHtml());
+      }
+      
+    });
+  }
 }
